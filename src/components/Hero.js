42fc6fe6ac70bd13d15b17274b172f46e90e59e5
Macintosh <pmotiuk@Macintoshs-Mac-Pro.local>
2022-12-08 00:16:07 +0100

    website1

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -29,13 +29,12 @@ position: relative;
 `;
 
 const HeroSlide = styled.div`
-
 width: 100%;
 height: 100%;
 `;
 const HeroSlider = styled.div`
 position: absolute;
-z-index: 999;
+z-index: 10;
 top: 0;
 left: 0;
 width: 100%;
@@ -155,7 +154,7 @@ useEffect(() => {
         setCurrent(current => (current === length - 1 ? 0 : current +1))
     }
 
-    timeout.current = setTimeout(nextSlide, 99990000)
+    timeout.current = setTimeout(nextSlide, 99999990000)
 
     return function () {
         if(timeout.current) {
